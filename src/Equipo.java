6d588a890df8cf624494e019f61e7c91f5670db9
AntonioForneron <AntonioForneron@Satellite>
2014-06-16 08:18:49 -0400

    1er

---
+++ src/Equipo.java
@@ -0,0 +1,47 @@
+
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AntonioForneron
+ */
+@Entity
+public class Equipo {
+    @Id @GeneratedValue int id;
+    String nombre;
+    @ManyToMany(mappedBy="Equipos")
+    Set<Torneo> Torneos;
+
+    public Equipo(String Nombre) {
+        this.nombre = Nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Set<Torneo> getTorneos() {
+        return Torneos;
+    }
+
+    public void setTorneos(Set<Torneo> Torneos) {
+        this.Torneos = Torneos;
+    }
+    
+    
+    
+}
---
+++ src/Torneo.java
@@ -0,0 +1,57 @@
+
+import java.util.Set;
+import javax.jdo.annotations.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AntonioForneron
+ */
+@Entity
+public class Torneo {
+    @Id
+    @Column (name="nomTorneo")
+    String nomTorneo;
+        @ManyToMany
+        @JoinTable(name="Torneo_Equipo", 
+        joinColumns= 
+                @JoinColumn(name="NombreTorneo", referencedColumnName="nomTorneo"),
+        inverseJoinColumns=
+                @JoinColumn(name="Id_Equipo", referencedColumnName="id")
+        )
+    Set<Equipo> Equipos;
+
+    public Torneo(String nomTorneo) {
+        this.nomTorneo = nomTorneo;
+    }
+
+    public Torneo() {
+    }
+
+    public String getNomTorneo() {
+        return nomTorneo;
+    }
+
+    public void setNomTorneo(String nomTorneo) {
+        this.nomTorneo = nomTorneo;
+    }
+
+    public Set<Equipo> getEquipos() {
+        return Equipos;
+    }
+
+    public void setEquipos(Set<Equipo> eq) {
+        this.Equipos=eq;
+    }
+        
+}
